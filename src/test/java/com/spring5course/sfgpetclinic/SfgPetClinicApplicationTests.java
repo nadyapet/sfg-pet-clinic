b9a94d5b89a4ee010740c83a7512cb7464a325ca
nadyapet <nadezhda.i.petrova@gmail.com>
2021-02-04 15:09:34 +0200

    Initial commit.

---
+++ src/test/java/com/spring5course/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5course.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
